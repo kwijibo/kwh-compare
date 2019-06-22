b6a8667a2a3e6737dd18458a0933875ee3e469da
Keith Alexander <keithalexander@keithalexander.co.uk>
2019-06-22 12:04:00 +0100

    added new things

--- src/index.elm
+++ src/index.elm
@@ -8,8 +8,8 @@ import Browser
 import Css exposing (..)
 import Html
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css, href, src)
-import Html.Styled.Events exposing (onClick)
+import Html.Styled.Attributes exposing (class, css, href, min, src, type_, value)
+import Html.Styled.Events exposing (onClick, onInput)
 import Round
 
 
@@ -30,6 +30,8 @@ type PowerUseUnit
     | Hour
     | Minute
     | PopcornBag
+    | Bitcoin
+    | Gigabyte
 
 
 unitName : PowerUseUnit -> String
@@ -48,7 +50,7 @@ unitName x =
             "330ml drinks can"
 
         WashingCycle ->
-            "cycle of a "
+            "cycle"
 
         CupOfTea ->
             "cup"
@@ -59,18 +61,26 @@ unitName x =
         IPhone6 ->
             "iPhone 6"
 
+        Bitcoin ->
+            "bitcoin"
+
+        Gigabyte ->
+            "gigabyte"
+
 
 type alias EnergyUse =
     { label : String
     , useUnit : PowerUseUnit
     , wattHours : Float
     , info : String
+    , icon : String
     }
 
 
 type alias Model =
     { selected : EnergyUse
     , energyUses : List EnergyUse
+    , quantity : Int
     }
 
 
@@ -81,72 +91,70 @@ type alias Entry =
     }
 
 
-makingDrinksCan =
-    EnergyUse "made from new aluminium" DrinksCan 140 "recycled aluminium would require only 5% of the energy"
-
-
-lowEnergyTV =
-    EnergyUse "watching TV" Hour 50 "A++ TV consuming 50w an hour"
-
-
-ledBulb =
-    EnergyUse "lighting an LED spotlight" Hour 5 "5w spot light, providing 346 lumens, equivalent to a 50w incandescent"
-
-
-fridgeFreezer =
-    EnergyUse "cooling a fridge-freezer" Hour 18.26 "based on a Miele A+++ 300l Fridge-Freezer"
-
-
-ebike =
-    EnergyUse "riding an e-bike" Mile 3.9 "based on a Bosch ActiveLine in eco-mode"
-
-
+electricCar : EnergyUse
 electricCar =
-    EnergyUse "driving an electric car" Mile 250 "based on a Nissan Leaf: a Tesla Model S will go 72% of this distance; a Renault Zoe will go 38% further!"
+    EnergyUse "driving an electric car" Mile 250 "Car energy efficiency figures based on a Nissan Leaf. A Tesla Model S will go 72% of this distance; a Renault Zoe will go 38% further! A 45pmg petrol car will get only 25% of this distance" "car"
 
 
 init : Model
 init =
     Model electricCar
-        [ makingDrinksCan
-        , lowEnergyTV
-        , ledBulb
-        , fridgeFreezer
-        , ebike
-        , electricCar
-        , EnergyUse "of tea" CupOfTea 33 "boiling the water in a normal 3kw kettle"
-        , EnergyUse "of popcorn" PopcornBag 27 "popped in an 800w microwave"
-        , EnergyUse "cooking on an induction hob" Minute 33.333 "assuming the hob is on full power"
-        , EnergyUse "taking a shower" Minute 158.3333 "(electric shower 9.5kw)"
-        , EnergyUse "vacuuming" Minute 15 "based on a 900w compact vacuum cleaner"
-        , EnergyUse "charging to full" IPhone6 22 ""
-        , EnergyUse "using a laptop" Hour 58.4 "based on an Apple MacBook Pro 15\" (2.9 GHz)"
-        , EnergyUse "washing machine on Baby Care (60ºC)" WashingCycle 2200 ""
+        [ electricCar
+        , EnergyUse "lighting an LED spotlight" Hour 5 "5w spot light, providing 346 lumens, equivalent to a 50w incandescent" "bulb"
+        , EnergyUse "made from new aluminium" DrinksCan 140 "Recycled aluminium would require only 5% of the energy" "can"
+        , EnergyUse "cooling a fridge-freezer" Hour 18.26 "With a Miele A+++ 300l Fridge-Freezer. A modern fridge uses between 40 and 60% less energy than models sold before 2000. An A+ model uses 20% less energy than an A-rated fridge-freezer. An A++ uses about 40% less. Stacked fridge-freezers are about 20% more efficient than American-style side-stacked models." "fridge"
+        , EnergyUse "riding an e-bike" Mile 10.989 "With a Bosch Active Line drive system, used in \"Turbo\" mode, with an average speed of 14mph. In \"eco\" mode, it would use less than 40% of this energy - about 4 watts a mile. \n Some calculations show that using land for solar panels to power e-bikes can get people more than 800x as far as using the same land to produce calories for the cyclist of a traditional manual bicycle (see Mike Berners-Lee's ❝There is no Planet B❞, 2019)." "bike"
+        , EnergyUse "watching TV" Hour 50 "With an A++ TV consuming 50w an hour" "tv"
+        , EnergyUse "of tea" CupOfTea 33 "Boiling only the water required, in a normal 3kW kettle. Kettles use about 6% of all the electricity in UK homes." "kettle"
+        , EnergyUse "of popcorn" PopcornBag 27 "Assuming one bag takes 2 minutes in an 800w microwave" "microwave"
+        , EnergyUse "cooking on an induction hob" Minute 33.333 "With a 2kW induction hob. Induction hobs have been found to use 60-40% less energy compared to a gas hob." "cooker"
+        , EnergyUse "taking a shower" Minute 158.3333 "With a 9.5kW electric shower." "shower"
+        , EnergyUse "vacuuming" Minute 15 "With a 900w compact vacuum cleaner" "vacuum"
+        , EnergyUse "charging to full" IPhone6 22 "Newer larger iPhones require more energy to charge" "phone"
+        , EnergyUse "using a laptop" Hour 58.4 "Based on an Apple MacBook Pro 15\" (2.9 GHz)" "laptop"
+        , EnergyUse "of a washing machine on Baby Care (60ºC)" WashingCycle 2200 "The cycle lasts for 3 hours." "washing"
+        , EnergyUse "powering a broadband router" Hour 6 "When you aren't using the internet, the idle consumption of a router might be as low as 2w, but under load, could be 20w ; 6w is used here as a rough average." "router"
+        , EnergyUse "mined" Bitcoin 69710806.6971 "\"The Carbon Footprint of Bitcoin\" (2019) calculates that Bitcoin's annual electricity consumption is 45.8 TWh, and that 1,800 bitcoins are mined a day." "bitcoin"
+        , EnergyUse "electricity in the average UK home" Hour 429.2237 "The average UK household uses about 3760 kWH annually." "house"
+        , EnergyUse "electricity in the average US home" Hour 1404.109589 "The average US household uses about 12,300 kWH annually." "house"
+        , EnergyUse "electricity in the average Chinese home" Hour 171.233 "The average Chinese household uses about 1,500 kWH annually." "house"
+        , EnergyUse "of data downloaded through an ISP" Gigabyte 21.2 "Netflix, on a Medium quality setting, streams at `up to 0.7GB an hour`" "data"
+        , EnergyUse "of a tumble dryer" WashingCycle 1670 "Based on getting 8kg of cotton `cupboard dry` in 130 minutes in an AEG Lavatherm A++ tumble dryer" "washing"
         ]
+        1
 
 
 
 -- UPDATE
 
 
-type alias Msg =
-    EnergyUse
+type Msg
+    = ChangeEnergyUse EnergyUse
+    | ChangeQuantity Int
 
 
 update : Msg -> Model -> Model
 update msg model =
-    { model | selected = msg }
+    case msg of
+        ChangeEnergyUse energyUse ->
+            { model | selected = energyUse, quantity = 1 }
+
+        ChangeQuantity quantity ->
+            { model | quantity = quantity }
 
 
 roundish : Float -> String
 roundish n =
     if n < 0.9 then
-        -- Round.round 2 n
         Round.round 0 (n * 100) ++ "%"
-    else if n < 9 then
-        Round.round 1 n
+    else if (n < 2) && (n /= toFloat (Basics.round n)) then
+        Round.round 0 n
     else
         Round.round 0 n
+
+
+worldCircumference =
+    24901.55
 
 
 makeEntry selectedWattHours energyUse =
@@ -160,6 +168,12 @@ makeEntry selectedWattHours energyUse =
                 Entry energyUse (num * 60 * 60) "second"
             else if num < 1 then
                 Entry energyUse (num * 60) "minute"
+            else if num >= (24 * 365) then
+                Entry energyUse (num / (24 * 365)) "year"
+            else if num >= (24 * 30) then
+                Entry energyUse (num / (24 * 30)) "month"
+            else if num >= (24 * 7) then
+                Entry energyUse (num / (24 * 7)) "week"
             else if num >= 24 then
                 Entry energyUse (num / 24) "day"
             else
@@ -175,6 +189,23 @@ makeEntry selectedWattHours energyUse =
             else
                 Entry energyUse num "minute"
 
+        Gigabyte ->
+            if num < (1 / 1024) then
+                Entry energyUse (num * 1024 * 1024) "kilobyte"
+            else if num < 1 then
+                Entry energyUse (num * 1024) "megabyte"
+            else if num > 1024 then
+                Entry energyUse (num / 1024) "terrabyte"
+            else
+                Entry energyUse num (unitName energyUse.useUnit)
+
+        {--
+        Mile ->
+            if num > worldCircumference then
+                Entry energyUse (num / worldCircumference) "x around the world"
+            else
+                Entry energyUse num (unitName energyUse.useUnit)
+                --}
         _ ->
             Entry energyUse num (unitName energyUse.useUnit)
 
@@ -185,23 +216,69 @@ makeEntry selectedWattHours energyUse =
 
 view : Model -> Html Msg
 view model =
-    div [ css [ color (rgb 14 15 16), fontSize (pt 22), fontFamilies [ "Impact", "Trebuchet MS", "sans-serif" ] ] ]
+    div [ css [ width (Css.pct 85), margin auto, color (rgb 14 15 16), fontSize (pt 18), fontFamilies [ "Impact", "Trebuchet MS", "sans-serif" ] ] ]
         [ p
             []
-            [ bigStrong "1 "
-            , text (unitName model.selected.useUnit ++ " ")
+            [ input
+                [ css
+                    [ width (Css.em 2)
+                    , marginLeft (Css.em 0.5)
+                    , margin (Css.em 0.5)
+                    , padding (Css.em 0.125)
+                    , fontSize (pt 38)
+                    , fontWeight bold
+                    , color (hex "#5779A8")
+                    ]
+                , type_ "number"
+                , min "1"
+                , value (String.fromInt model.quantity)
+                , onInput
+                    (\n -> ChangeQuantity (Maybe.withDefault 1 (String.toInt n)))
+                ]
+                []
+            , text (pluralise (unitName model.selected.useUnit) model.quantity ++ " ")
             , strong [ css [ fontSize larger, fontWeight lighter, color (hex "#5779DD"), padding (Css.em 0.25) ] ] [ text model.selected.label ]
+            , makeIcon model.selected.icon
             , text " uses the same energy as:"
+            ]
+        , div
+            [ css
+                [ fontStyle italic
+                , float right
+                , width (Css.em 20)
+                , fontSize (Css.pt 14)
+                , fontWeight lighter
+                , fontFamilies [ "Arial", "Trebuchet MS", "sans-serif" ]
+                , marginRight (Css.pct 15)
+                , border3 (px 5) dotted (rgb 11 14 17)
+                , padding (px 15)
+                ]
+            ]
+            [ br [] []
+            , h1
+                [ css
+                    [ color (hex "#149FEB")
+                    , fontSize (Css.pt 70)
+                    , fontStyle normal
+                    , padding (Css.px 0)
+                    , margin (Css.px 0)
+                    , marginTop (Css.em -0.5)
+                    , float left
+                    ]
+                ]
+                [ text "ℹ"
+                ]
+            , span [] [ text model.selected.info ]
             ]
         , div []
             (model.energyUses
                 |> List.filter (\eu -> eu /= model.selected)
-                |> List.map (makeEntry model.selected.wattHours)
+                |> List.map (makeEntry (model.selected.wattHours * toFloat model.quantity))
                 |> List.sortBy .num
                 |> List.reverse
                 |> List.map
                     (\entry ->
-                        div [ css [] ]
+                        div [ class entry.energyUse.icon, css [] ]
                             [ bigStrong (roundish entry.num)
                             , text
                                 ((if entry.num < 1 then
@@ -217,13 +294,39 @@ view model =
                                        )
                                 )
                             , a
-                                [ onClick entry.energyUse, href "#", css [ color (hex "#5779B7") ] ]
-                                [ text entry.energyUse.label ]
+                                [ onClick (ChangeEnergyUse entry.energyUse)
+                                , href "#"
+                                , css
+                                    [ color (hex "#5779B7")
+                                    , textDecoration none
+                                    ]
+                                ]
+                                [ text (entry.energyUse.label ++ "  ")
+                                , makeIcon entry.energyUse.icon
+                                ]
                             ]
                     )
             )
         ]
 
 
+makeIcon name =
+    img
+        [ src ("icons/" ++ name ++ ".png")
+        , css
+            [ marginBottom (Css.px -6)
+            , width (Css.em 1.5)
+            ]
+        ]
+        []
+
+
 bigStrong txt =
     strong [ css [ fontSize (pt 38), color (hex "#5779A8"), padding (Css.em 0.25) ] ] [ text txt ]
+
+
+pluralise name quantity =
+    if quantity == 1 then
+        name
+    else
+        name ++ "s"
